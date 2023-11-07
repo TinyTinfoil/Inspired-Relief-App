c86b789fa20e7ec20604823679f430ee87b974a4
Tinytinfoil <55459863+TinyTinfoil@users.noreply.github.com>
2023-11-07 09:47:55 -0500

    Web View Experiment

--- helper_card.tsx
+++ helper_card.tsx
@@ -1,20 +1,21 @@
 import { FlatList, StyleSheet, Text, View, Image, Dimensions } from 'react-native';
 import { ResizeMode, Video } from 'expo-av';
-import VideoPlayer  from 'expo-video-player'
+// import VideoPlayer  from 'expo-video-player'
+import WebView from 'react-native-webview';
 export function HelperCard({name, description, video, contact}) {
   return (
     <View style={styles.container}>
         <Text style={styles.title}>{name}</Text>
         <VideoPlayer
           videoProps={{
-          source:{ uri: video },
-          resizeMode: ResizeMode.CONTAIN,
-          shouldPlay: true,
+          source:{ uri: video }//,
+          //resizeMode: ResizeMode.CONTAIN,
+          //shouldPlay: true,
           // style: {margin: 'auto'}
         }}
           
-          style={{ width: Dimensions.get('window').width - 40 , height: 200, videoBackgroundColor: 'transparent',
-          controlsBackgroundColor: 'red',}}
+          // style={{ width: Dimensions.get('window').width - 40 , height: 200, videoBackgroundColor: 'transparent',
+          // controlsBackgroundColor: 'red',}}
         />
         {/* <Video 
         source={{uri: video}}
@@ -64,3 +65,14 @@ const styles = StyleSheet.create({
         float: 'left'
     }
 });
+
+function VideoPlayer(props: { videoProps: { source: { uri: string; }; }; } ) {
+  return (
+    <WebView
+      style={{ width: Dimensions.get('window').width - 40 , height: 200}}
+      javaScriptEnabled={true}
+      domStorageEnabled={false}
+      source={{ html: `<iframe width="640" height="360" src="https://www.youtube.com/embed/dQw4w9WgXcQ" title="Rick Astley - Never Gonna Give You Up (Official Music Video)" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>` }}
+    />
+  );
+}
